47bb24cb88f2c20fde7c569e94e37d42a45eb05d
Adrian Setiyadi <adrianpros27@gmail.com>
2022-08-08 17:05:37 +0700

    exercise 1

---
+++ marsiajar_java/Exercise_01/WriteSimpleProgram/WriteSimpleProgram.java
@@ -0,0 +1,15 @@
+package marsiajar_java.Exercise_01.WriteSimpleProgram;
+
+
+/*
+    * (Display three messages) Write a program that displays Welcome to Java,
+    *  Welcome to Computer Science, and Programming is fun.
+*/
+
+public class WriteSimpleProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is FUN");
+    }
+}
