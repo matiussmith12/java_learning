49aea60d1cd2a9e195d34215f1238c0cbe7e08b7
Adrian Setiyadi <adrianpros27@gmail.com>
2022-08-08 17:05:55 +0700

    exercise 1b

---
+++ marsiajar_java/Exercise_01/Exercise_01_B/Exercise_01_B.java
@@ -0,0 +1,15 @@
+package marsiajar_java.Exercise_01.Exercise_01_B;
+
+
+
+// TODO: (Display five messages) Write a program that displays Welcome to Java five times.
+public class Exercise_01_B {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        
+    }
+}
